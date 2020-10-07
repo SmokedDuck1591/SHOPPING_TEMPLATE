022d910b1cf9369ef693deec95aa2833a4071206
SmokedDuck1591 <coutinho159123@gmail.com>
2020-10-08 00:14:39 +0900

    maybe finish

--- src/main.js
+++ src/main.js
@@ -21,6 +21,34 @@ function createHTMLString(item) {
       </li>  
     `
 }
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;
+    }
+    updateItems(items, key, value);
+}
+
+function updateItems(items, key, value) {
+    items.forEach(item => {
+        if(item.dataset[key] === value) {
+            item.classList.remove('invisible');
+        } else {
+            item.classList.add('invisible');
+        }
+
+    })
+}
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
 // main
 loadItems() 
   .then(items => {
